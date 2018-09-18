0965df222469baa03da078487ac7166539e2fad8
Richard Le Poidevin <ric@wearebwi.com>
2018-09-18 14:29:43 +0100

    Fix JS loading when dialog is not in DOM (was previously accepted)

--- src/views/dialog.blade.php
+++ src/views/dialog.blade.php
@@ -11,20 +11,20 @@
 			</button>
 		</div>
 	</div>
+
+	<script>
+		window.cookieLaw = (function() {
+			function acceptCookies() {
+				var date = new Date();
+				date.setTime(date.getTime() + ({{ Config::get('cookie-law::cookie-law.cookie_lifetime') }} * 24 * 60 * 60 * 1000));
+				document.cookie = '{{ Config::get('cookie-law::cookie-law.cookie_name') }}=1; ' + 'expires=' + date.toUTCString() +';';
+
+				var dialog = document.querySelector('.js-cookie-law-dialog');
+				dialog.classList.add('cookie-law--accepted')
+			}
+
+			var acceptButton = document.querySelector('.js-cookie-law-accept');
+			acceptButton.addEventListener('click', acceptCookies);
+		})();
+	</script>
 @endif
-
-<script>
-	window.cookieLaw = (function() {
-		function acceptCookies() {
-			var date = new Date();
-			date.setTime(date.getTime() + ({{ Config::get('cookie-law::cookie-law.cookie_lifetime') }} * 24 * 60 * 60 * 1000));
-			document.cookie = '{{ Config::get('cookie-law::cookie-law.cookie_name') }}=1; ' + 'expires=' + date.toUTCString() +';';
-
-			var dialog = document.querySelector('.js-cookie-law-dialog');
-			dialog.classList.add('cookie-law--accepted')
-		}
-
-		var acceptButton = document.querySelector('.js-cookie-law-accept');
-		acceptButton.addEventListener('click', acceptCookies);
-	})();
-</script>
